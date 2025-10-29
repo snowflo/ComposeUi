53e2132d20dd5bc36d9dc56b84a928cf44b9523a
DESKTOP-FJ8UU01\snowflo <g175151@gmail.com>
2025-10-29 21:50:49 +0900

    glide library를 활용한 이미지 지정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.glide)  // 이미지 처리를 위한 glide 추가
 }
--- app/src/main/java/com/example/composeuisample/sample/ListSampleActivity.kt
+++ app/src/main/java/com/example/composeuisample/sample/ListSampleActivity.kt
@@ -1,12 +1,14 @@
 package com.example.composeuisample.sample
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,7 +17,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -27,15 +28,25 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.composeuisample.R
 import com.example.composeuisample.model.Sample
 import com.example.composeuisample.model.SampleProvider
@@ -132,18 +143,7 @@ fun SampleView(sample: Sample) {
             verticalAlignment = Alignment.CenterVertically,  // Row기 때문에 CenterVertically 가능
             horizontalArrangement = Arrangement.spacedBy(10.dp) // 아이템끼리 공간 주기
         ) {
-            Box(
-                modifier = Modifier
-                    .size(width = 100.dp, height = 100.dp)
-                    .clip(CircleShape)
-                    .background(Color.Red)
-            )
-            Box(
-                modifier = Modifier
-                    .size(width = 100.dp, height = 100.dp)
-                    .clip(CircleShape)
-                    .background(Color.Green)
-            )
+            ProfileImage(imgUrl = sample.profileImg)
             Column() {
                 Text(
                     text = sample.name,
@@ -158,6 +158,46 @@ fun SampleView(sample: Sample) {
             }
         }
     }
+}
+
+@Composable
+fun ProfileImage(imgUrl: String, modifier: Modifier = Modifier) {
+    // 이미지 비트맵 저장
+    val bitmap: MutableState<Bitmap?> = remember { mutableStateOf(null) }
+    val imageModifier = modifier
+        .size(width = 50.dp, height = 50.dp)
+        .clip(RoundedCornerShape(10.dp))
+
+    // LocalContext: 현재 컨텍스트를 가져옴
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(imgUrl)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                // 이미지를 비트맵으로 받아 준비 완료
+                // Glide 라이브러리를 통해 다운받은 비트맵
+                bitmap.value = resource
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+
+            }
+        })
+
+    // 비트맵이 있다면
+    bitmap.value?.asImageBitmap()?.let { fetchedBitmap ->
+        Image(
+            bitmap = fetchedBitmap,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = imageModifier
+        )   // 비트맵으로 이미지 그리기
+    } ?: Image(
+        painter = painterResource(id = R.drawable.ic_empty_user_img),
+        contentDescription = null,
+        contentScale = ContentScale.Fit,
+        modifier = imageModifier
+    )   // 페인터로 이미지 그리기
 }
 
 fun getSampleBackground(): Color {
